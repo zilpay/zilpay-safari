00bd1f1764db88677ad469a40e5a5461c78510bf
Rinat <lich666black@gmail.com>
2021-12-06 11:53:42 +0300

    Initial Commit

---
+++ ZIlPay/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  ZIlPay
+//
+//  Created by Ринат Хасаншин on 06.12.2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ notification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
